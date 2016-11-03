4c72687e7e3f4dfddda6a3f6d2a8696ea6d46279
asingularity <csaba.petre@gmail.com>
2016-11-02 19:18:40 -0700

    simple raycasting

--- robot_environment.py
+++ robot_environment.py
@@ -4,60 +4,58 @@ from utils import sintable, costable
 
 class RobotEnvironment(object):
     def __init__(self, params):
-        W = 24  # Width/Height
-        H = 79
+        self.W = 24  # Width/Height
+        self.H = 79
 
-        RAYS = 360  # Should be 360!
-
-        STEP = 6  # The step of for cycle. More = Faster, but large steps may
+        self.RAYS = 360  # Should be 360!
+        self.STEP = 6  # The step of for cycle. More = Faster, but large steps may
         # cause artifacts. Step 3 is great for radius 10.
 
-        RAD = 100  # FOV radius.
+        self.RAD = 100  # FOV radius.
 
         self.level = []
         self.view = []
 
-        for x in range(W + 1):
+        for x in range(self.W + 1):
             self.view.append([])
             self.level.append([])
 
-            for y in range(H + 1):
-                fov[x].append(False)
-                level[x].append(False)  # Every tile is floor
+            for y in range(self.H + 1):
+                self.view[x].append(False)
+                self.level[x].append(False)  # Every tile is floor
 
-        level[5][5] = 1  # Add some walls
-        level[11][11] = 1
-        level[10][16] = 1
-        level[11][17] = 1
+        self.level[5][5] = 1  # Add some walls
+        self.level[11][11] = 1
+        self.level[10][16] = 1
+        self.level[11][17] = 1
 
-        px = 10  # Player coordinates
-        py = 10
+        self.px = 10  # Player coordinates
+        self.py = 10
 
-        camera_image = np.zeros(len(range(0, RAYS + 1, STEP)))
-
+        self.camera_image = np.zeros(len(range(0, self.RAYS + 1, self.STEP)))
 
     def step_environment(self, robot_model):
 
         pixel_index = -1
-        for i in range(0, RAYS + 1, STEP):
+        for i in range(0, self.RAYS + 1, self.STEP):
             pixel_index += 1
             ax = sintable[i]  # Get precalculated value sin(x / (180 / pi))
             ay = costable[i]  # cos(x / (180 / pi))
 
-            x = px  # Player's x
-            y = py  # Player's y
+            x = self.px  # Player's x
+            y = self.py  # Player's y
 
-            camera_image[pixel_index] = 0
-            for z in range(RAD):  # Cast the ray
+            self.camera_image[pixel_index] = 0
+            for z in range(self.RAD):  # Cast the ray
                 x += ax
                 y += ay
 
-                if x < 0 or y < 0 or x > W or y > H:  # If ray is out of range
-                    camera_image[pixel_index] = 0
+                if x < 0 or y < 0 or x > self.W or y > self.H:  # If ray is out of range
+                    self.camera_image[pixel_index] = 0
                     break
 
-                fov[int(round(x))][int(round(y))] = 1  # Make tile visible
+                self.view[int(round(x))][int(round(y))] = 1  # Make tile visible
 
-                if level[int(round(x))][int(round(y))] == 1:  # Stop ray if it hit
-                    camera_image[pixel_index] = 1
+                if self.level[int(round(x))][int(round(y))] == 1:  # Stop ray if it hit
+                    self.camera_image[pixel_index] = 1
                     break  # a wall.
--- visualizer.py
+++ visualizer.py
@@ -1,4 +1,5 @@
 import time
+import sys
 import logging
 logging.basicConfig(level=logging.INFO)
 
@@ -18,12 +19,19 @@ class Visualizer(object):
             self.last_FPS_time = time.time()
         self.fps_frames += 1
 
-    def _display_text_map(self):
+    def _display_text_map(self, robot_environment):
+        W = robot_environment.W
+        H = robot_environment.H
+        view = robot_environment.view
+        level = robot_environment.level
+        px = robot_environment.px
+        py = robot_environment.py
+
         for x in range(W + 1):
             for y in range(H + 1):
                 if (x, y) == (px, py):
                     sys.stdout.write("@")  # Player
-                elif fov[x][y] == 1:
+                elif view[x][y] == 1:
                     if level[x][y] == 0:  # Seen
                         sys.stdout.write(".")
                     else:
@@ -33,5 +41,8 @@ class Visualizer(object):
             print
 
     def visualize(self, robot_sensors, robot_brain, robot_model, robot_environment):
-        self._display_text_map()
+        if self.frames % 5000 == 0:
+            self._display_text_map(robot_environment)
         self._display_fps()
+
+        self.frames += 1
