one bug left in raycasting: radian discontinuity?

robot_sensors.py
@@ -13,11 +13,13 @@ class RobotSensors(object):
         self.ray_radians = (pi / 180.0) * self.ray_degrees
         self.relative_ray_radians = self.ray_radians.copy()
         self.ray_colors = np.zeros(num_rays)
+        self.ray_lengths = np.zeros(num_rays)
 
     def get_rays(self):
         return {
             'ray_radians': self.relative_ray_radians,
-            'ray_colors': self.ray_colors
+            'ray_colors': self.ray_colors,
+            'ray_lengths': self.ray_lengths
         }
 
     def read_input(self, robot_environment):
@@ -39,16 +41,18 @@ class RobotSensors(object):
         for k in range(relative_ray_radians.shape[0]):
             ray_theta = relative_ray_radians[k]
 
-            # TODO ray_theta needs normalization to same range as theta. potentially circular for some tiles!
-            # TODO for that we need to see what min, max of precomputed thetas are
-
+            # TODO does this check need circular checking?
             matching_tile_indices = np.nonzero(np.logical_and(ray_theta > theta - d_theta, ray_theta < theta + d_theta))[0]
             if matching_tile_indices.shape[0] > 0:
                 matching_tile_colors = color[matching_tile_indices]
                 matching_tile_dists = dist[matching_tile_indices]
-
-                self.ray_colors[k] = matching_tile_colors[np.argmin(matching_tile_dists)]
+                min_tile = np.argmin(matching_tile_dists)
+                self.ray_colors[k] = matching_tile_colors[min_tile]
+                self.ray_lengths[k] = matching_tile_dists[min_tile]
             else:
                 self.ray_colors[k] = 0
+
+                # TODO make this a parameter (max ray length):
+                self.ray_lengths[k] = 1000
 
         self.relative_ray_radians = relative_ray_radians