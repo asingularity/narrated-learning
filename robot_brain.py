568bb1ab6e10d4e6b0ae0d6341c9a6d72cd57dfa
redpanda <a@a.com>
2017-02-15 20:23:18 -0800

    a

--- robot_brain.py
+++ robot_brain.py
@@ -557,7 +557,7 @@ class RobotBrain(object):
             inv = self.inverse_list[0]  # TODO select inverse model here
             self.motor_out = inv.lookup_motor_to_goal(goal_states, self.states_history)
             print 'motor_out, no index: ', self.motor_out
-            self.motor_out = self.motor_out[1]  # TODO this depends on which inverse model
+            self.motor_out = self.motor_out[1]  # TODO this depends on which inverse model?
         else:
             self.motor_out = None
 
