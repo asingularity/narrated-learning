9fcf893c09d69a6f43f3d90583bc9646391f8d1a
asingularity <csaba.petre@gmail.com>
2018-11-02 17:16:59 -0700

    all-to-all array is way to go for all-all dist

--- sorteddict_speed_test.py
+++ sorteddict_speed_test.py
@@ -1,9 +1,10 @@
+import numpy as np
 import random
 from time import time
-from sortedcontainers import SortedDict
 
 
 def test_sorted_dict():
+    from sortedcontainers import SortedDict
     rows = 2000
 
     a = SortedDict()
@@ -24,5 +25,36 @@ def test_sorted_dict():
 
     print('FPS: ', frames * 1.0 / (t1 - t0))
 
+
+def test_all_all_array():
+    rows = 20000
+    d = np.random.random((rows, rows))
+    actual_min = np.amin(d)
+    print(d.shape)
+    print('min', actual_min)
+    print()
+    print('Start...')
+
+    t0 = time()
+    frames = 5000
+    for k in range(frames):
+        # choose a random row, and replaces all distances to/from it with a new float array
+        rep_row_ind = random.randint(1, rows - 1)
+        new_row = np.random.random(rows)[:]
+        d[rep_row_ind, :] = new_row
+        d[:, rep_row_ind] = new_row
+
+        min_d = np.amin(new_row)
+        if min_d < actual_min:
+            actual_min = min_d
+        #(random.randint(1, rows - 1), random.randint(1, rows - 1))
+    t1 = time()
+
+    print('FPS: ', frames * 1.0 / (t1 - t0))
+    print()
+    print('min', actual_min)
+
+
 if __name__ == '__main__':
-    test_sorted_dict()
+    #test_sorted_dict()
+    test_all_all_array()
