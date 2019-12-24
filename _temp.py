706a9fd401a008fd08b3c4c1df923dc8da53a90b
asingularity <csaba@getcleverpet.com>
2019-12-24 13:50:30 -0800

    blasd

--- _temp.py
+++ _temp.py
@@ -11,14 +11,24 @@ import skcuda.misc as misc
 from utils.fps_counter import FPSCounter
 
 if __name__ == '__main__':
-    X = np.random.random((1024, 48)).astype(np.float32)
+
+    input_dim = 48
+    num_entries = 8000
+
+    table_i_only = np.random.random((num_entries, input_dim)).astype(np.float32)
+
+    table_i_gpu = gpuarray.to_gpu(np.ascontiguousarray(np.transpose(table_i_only)))
+    i_d_t_gpu = table_i_gpu
 
     fps = FPSCounter(params={'display_every_k_seconds': 5})
     linalg.init()
 
     while True:
+        X = np.random.random((1024, input_dim)).astype(np.float32)
+
         X_gpu = gpuarray.to_gpu(X)
-        Y = (-2 * X_gpu).get()
+
+        term_1 = (-2 * linalg.dot(X_gpu, i_d_t_gpu)).get()
         fps.update()
         #print()
         #print(X[0:4, 0:4])
