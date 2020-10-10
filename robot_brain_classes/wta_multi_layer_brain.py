284a9fab6c3d6aae41d20907f28d269cedb85474
asingularity <csaba.petre@gmail.com>
2020-10-09 18:07:09 -0700

    new viz for second layer. something weird! weights for all delay steps are always exactly the same!

--- robot_brain_classes/wta_multi_layer_brain.py
+++ robot_brain_classes/wta_multi_layer_brain.py
@@ -142,9 +142,11 @@ class WTAMultiLayerBrain(object):
             self.rec_error.append(np.zeros(max_time))
             self.mean_rec_error.append(np.zeros(max_time))
 
+            print()
             hl_weights = []
             for k in range(self.num_wta_layers_per_hl[hl]):
                 hl_weights.append(np.zeros((self.num_rf_per_wta_layer_per_hl[hl], hl_input_state_dim)))
+                print('    adding weights for wta layer:', k, ', of shape (num rf per wta layer, input dim):', (self.num_rf_per_wta_layer_per_hl[hl], hl_input_state_dim))
 
             self.weights.append(hl_weights)
 
@@ -590,42 +592,105 @@ class WTAMultiLayerBrain(object):
             #   idea: instead, we could i.e. show N "winning" sample input sequences for each RF
             #       (this would be very noisy...)
 
-            if False:
+            hl_1_im_v = None
+            hl_1_im_w = None
 
-                for wta_layer in range(self.num_wta_layers_per_hl[hl]):
-                    arr, weights = self._collapse_binned_columns_to_pixels(arr_exp=self.weights[hl][wta_layer],
-                                                                           num_bins_per_pixel=self.bins_per_pixel)
+            for wta_layer in range(self.num_wta_layers_per_hl[hl]):
+                #arr, weights = self._collapse_binned_columns_to_pixels(arr_exp=self.weights[hl][wta_layer],
+                #                                                       num_bins_per_pixel=self.bins_per_pixel)
+                weights = self.weights[hl][wta_layer]
 
-                    assert weights.shape[0] == self.num_rf_per_wta_layer_per_hl[hl]
+                assert weights.shape[0] == self.num_rf_per_wta_layer_per_hl[hl]
+                assert weights.shape[1] == self.num_wta_layers_per_hl[hl - 1] * self.num_rf_per_wta_layer_per_hl[hl - 1] * self.input_time_steps_per_hl[hl]
 
-                    # compute for each RF in this second layer:
-                    for rf_ind in range(self.num_rf_per_wta_layer_per_hl[hl]):
-                        rf_weights = weights[rf_ind, :]
+                wta_vstacked_im_v = None
+                wta_vstacked_im_w = None
 
-                        # TODO what's not right here? only getting input from one WTA layer of previous hl!
-                        # rf_weights.shape[0]: 80, which is 20 (RFs) * 4 (time steps)
+                # compute for each RF in this second layer:
+                for rf_ind in range(self.num_rf_per_wta_layer_per_hl[hl]):
+                    rf_weights = weights[rf_ind, :]
 
-                        k = 0
+                    k = 0
+
+                    im_t_delay_hstack_v = None
+                    im_t_delay_hstack_w = None
+
+                    for time_del in range(self.input_time_steps_per_hl[hl]):
+                        # now we need to weighted-add the hl==0 RFs, weighing with these corresponding RF weights
+                        # use same hack enable option to leave out rf 0
+                        num_weights_step = self.num_wta_layers_per_hl[hl - 1] * self.num_rf_per_wta_layer_per_hl[hl - 1]
+
+                        rf_weights_step = rf_weights[k:k + num_weights_step]
+                        rf_to_add = None
+
+                        k2 = 0
+                        for prev_wta_ind in range(self.num_wta_layers_per_hl[hl - 1]):
+                            if (not self.enable_hack_skip_first_layer) or prev_wta_ind > 0:
+
+                                for prev_rf_ind in range(self.num_rf_per_wta_layer_per_hl[hl - 1]):
+
+                                    rf_tmp = self.weights[hl - 1][prev_wta_ind][prev_rf_ind, :]
+                                    if rf_to_add is None:
+                                        rf_to_add = rf_weights[k2] * rf_tmp
+                                    else:
+                                        rf_to_add = rf_to_add + rf_weights[k2] * rf_tmp
+                                    k2 += 1
+
+                        k += num_weights_step
+
                         # hstack by time delay
+                        tmp_im_v, tmp_im_w = self._collapse_binned_columns_to_pixels(arr_exp=rf_to_add[np.newaxis, :], num_bins_per_pixel=self.bins_per_pixel)
+                        tmp_im_v = tmp_im_v.reshape((self.input_im_dim, self.input_im_dim))
+                        tmp_im_w = tmp_im_w.reshape((self.input_im_dim, self.input_im_dim))
 
-                        for time_del in range(self.input_time_steps_per_hl[hl]):
-                            # now we need to weighted-add the hl==0 RFs, weighing with these corresponding RF weights
-                            # use same hack enable option to leave out rf 0
-                            num_weights_step = self.num_rf_per_wta_layer_per_hl[hl - 1]
+                        if im_t_delay_hstack_v is None:
+                            im_t_delay_hstack_v = tmp_im_v.copy()
+                            im_t_delay_hstack_w = tmp_im_w.copy()
+                        else:
+                            spacer = 0.5 * np.ones((tmp_im_v.shape[0], 2))
+                            im_t_delay_hstack_v = np.hstack((im_t_delay_hstack_v, spacer, tmp_im_v))
+                            im_t_delay_hstack_w = np.hstack((im_t_delay_hstack_w, spacer, tmp_im_w))
 
-                            rf_weights_step = rf_weights[k:k + num_weights_step]
+                    assert k == rf_weights.shape[0], str((k, rf_weights.shape[0]))
 
-                            #rfs_weighted_sum = self.weights[hl - 1][]
+                    # vstack the RFs
+                    if wta_vstacked_im_v is None:
+                        wta_vstacked_im_v = im_t_delay_hstack_v.copy()
+                        wta_vstacked_im_w = im_t_delay_hstack_w.copy()
+                    else:
+                        spacer = 0.5 * np.ones((2, im_t_delay_hstack_v.shape[1]))
+                        wta_vstacked_im_v = np.vstack((wta_vstacked_im_v, spacer, im_t_delay_hstack_v))
+                        wta_vstacked_im_w = np.vstack((wta_vstacked_im_w, spacer, im_t_delay_hstack_w))
 
-                            k += num_weights_step
-
-                        assert k == rf_weights.shape[0], str((k, rf_weights.shape[0]))
-
-                        # vstack the RFs
-
-                    # hstack a large (half-RF size) spacer
-
+                if hl_1_im_v is None:
+                    hl_1_im_v = wta_vstacked_im_v.copy()
+                    hl_1_im_w = wta_vstacked_im_w.copy()
+                else:
                     # hstack the WTA layers
+                    spacer = 0.5 * np.ones((hl_1_im_v.shape[0], 8))
+                    hl_1_im_v = np.hstack((hl_1_im_v, spacer, wta_vstacked_im_v))
+                    hl_1_im_w = np.hstack((hl_1_im_w, spacer, wta_vstacked_im_w))
+
+            # resize hl_1_im_v
+            # append hl_1_im_v
+            # resize hl_1_im_w
+            # append hl_1_im_w
+
+            # scale this image:
+            max_dim = max(hl_1_im_v.shape[0], hl_1_im_v.shape[1])
+            imscale = self.im_dim / max_dim  # 0.2: full table, 2.0
+            hl_1_im_v = cv2.resize(hl_1_im_v, dsize=(0, 0), fx=imscale, fy=imscale, interpolation=cv2.INTER_NEAREST)
+
+            max_dim = max(hl_1_im_w.shape[0], hl_1_im_w.shape[1])
+            imscale = self.im_dim / max_dim  # 0.2: full table, 2.0
+            hl_1_im_w = cv2.resize(hl_1_im_w, dsize=(0, 0), fx=imscale, fy=imscale, interpolation=cv2.INTER_NEAREST)
+
+            ims_list.append(hl_1_im_v)
+            ims_names_list.append('v_RF_weights_hyperlayer_' + str(hl))
+
+            ims_list.append(hl_1_im_w)
+            ims_names_list.append('w_RF_weights_hyperlayer_' + str(hl))
+
 
         # generic (all hyperlayers)
         if self.enable_generic_weights_viz:
